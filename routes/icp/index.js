d2d46bfce45049aa60d3021ab685de91f48f85f1
满目山河 <46342554+xiaokonglongya@users.noreply.github.com>
2022-06-18 01:21:37 +0800

    添加icp备案

--- routes/icp/index.js
+++ routes/icp/index.js
@@ -4,16 +4,21 @@
 
 var express = require("express");
 var router = express.Router();
-
+const axios = require("axios");
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
   let params = req.query;
   const { url = "" } = params;
-  let result = {
-    code: 200,
-    url,
-  };
-  res.send(result);
+  if (!url) {
+    res.send({
+      code: 0,
+      meg: "url为空",
+    });
+  }
+  const { data } = await axios.get("https://api.oick.cn/icp/api.php", {
+    params: { url },
+  });
+  res.send(data);
 });
 
 module.exports = router;
